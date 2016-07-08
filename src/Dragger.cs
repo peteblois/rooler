6bee956b2d0f41118e661d4af8ed66dd9612f81c
Daniel Rose <daniel.rose@inform-software.com>
2016-07-08 11:37:45 +0200

    Fix for the positioning when multiple displays exist, where not all line up across the top.

--- src/Dragger.cs
+++ src/Dragger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 
@@ -9,9 +10,39 @@ namespace Rooler {
 		private FrameworkElement target;
 		private TranslateTransform offset = new TranslateTransform();
 		private Point lastMousePt;
-		
+
 		public Dragger(FrameworkElement target) {
 			this.target = target;
+
+			this.target.Loaded += (sender, args) =>
+			{
+				// Calculate the offset:
+				// With multiple displays, depending their relative positioning,
+				// simply centering the target at the top may be outside the visual area.
+				var currentScreen = Screen.FromPoint(System.Windows.Forms.Cursor.Position);
+				var fullScreen = ScreenShot.FullScreenBounds;
+
+				var centerOfAllScreensX = fullScreen.Width/2.0;
+				var centerOfAllScreensY = (double)fullScreen.Top;
+
+				var centerOfCurrentScreenX = currentScreen.Bounds.Left + currentScreen.Bounds.Width/2;
+				var centerOfCurrentScreenY = currentScreen.Bounds.Top;
+
+				// Calculate the offset (difference between the top-center of all screens and that of the current screen.
+				var xOffset = centerOfCurrentScreenX - centerOfAllScreensX;
+				var yOffset = centerOfCurrentScreenY - centerOfAllScreensY;
+
+				// Transform the WinForms pixels (system dpi) to WPF pixels (based on virtual 96dpi).
+				var source = PresentationSource.FromVisual(target);
+				if (source?.CompositionTarget != null)
+				{
+					xOffset = xOffset / source.CompositionTarget.TransformToDevice.M11;
+					yOffset = yOffset / source.CompositionTarget.TransformToDevice.M22;
+				}
+
+				this.offset.X = xOffset;
+				this.offset.Y = yOffset;
+			};
 
 			this.target.RenderTransform = this.offset;
 
@@ -34,7 +65,7 @@ namespace Rooler {
 			this.target.ReleaseMouseCapture();
 		}
 
-		private void HandleMouseMove(object sender, MouseEventArgs e) {
+		private void HandleMouseMove(object sender, System.Windows.Input.MouseEventArgs e) {
 			if (this.target.IsMouseCaptured) {
 				Point point = e.GetPosition(this.target);
 
